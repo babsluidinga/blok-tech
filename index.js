1e8e14859d057936d424000a487eca11a023e1f5
babsluidinga <babs.luidinga@hva.nl>
2021-02-16 16:40:30 +0100

    eslint, prettier,install + server opgezet

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-
-const express =require('express');
-const app = express();
-const port = 8000;  
+const express = require('express');
 const path = require('path');
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, './static/public')));
 
-app.use(express.static(path.join(__dirname, 'static/public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/public/index.html'));
+});
 
-app.get("/", (req, res)=> {
-  res.sendFile(__dirname + '/static/public/index.html')
-})
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/public/about.html'));
+});
 
-app.use((req,res)=> {
-    res.status(404).send("Page Not Found")
-})  
-
-app.listen(port, ()=> console.log("listening on port " + port))
+app.listen(port, () => {
+  console.log(`Express server is listening on port ${port}!`);
+});
