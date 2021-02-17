ddae577e42225e13adc1942c3bfaf3771c16f580
babsluidinga <babs.luidinga@hva.nl>
2021-02-17 15:12:40 +0100

    EJS files en partials gemaakt, 404 toegvoegd, routing in server.js

--- server.js
+++ server.js
@@ -4,19 +4,32 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// set view directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
+// serve static files
 app.use(express.static(path.join(__dirname, './static/public')));
 
+// define all routes
 app.get('/', (req, res) => {
   res.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/public/about.html'));
+  res.render('pages/about', { pageTitle: 'About' });
 });
 
+app.get('/login', (req, res) => {
+  res.render('pages/login', { pageTitle: 'login' });
+});
+
+// error handling
+app.get('*', (req, res) => {
+  res.status(404).render('pages/error', { pageTitle: 'oops..' });
+});
+
+// testing
 app.listen(port, () => {
   console.log(`Express server is now listening on port ${port}!`);
 });
